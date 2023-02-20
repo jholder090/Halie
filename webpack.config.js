0dea32ecc23199b151a7a2455833a8028fc36be1
aguilana <naguila.mandich@gmail.com>
2023-02-20 17:13:42 -0500

    package json set up for now

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const Dotenv = require("dotenv-webpack");
+module.exports = {
+  entry: ["./client/index.js"],
+  output: {
+    path: __dirname + "/public",
+    filename: "bundle.js",
+  },
+  context: __dirname,
+  devtool: "source-map",
+
+  // this allows .env to happen
+  plugins: [
+    new Dotenv({
+      path: ".env", // or '.env.local', '.env.[mode]', etc.
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        // this allows css styling loader
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+        },
+      },
+    ],
+  },
+};
