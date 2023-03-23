60cffe351bb58ce074cc4140323359d215bc15b8
Joe Holder <jholder090@yahoo.com>
2023-03-23 14:56:47 -0600

    user API routes done for now

--- server/api/users.js
+++ server/api/users.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const {
   models: { User, Cart, Order },
 } = require("../db");
+const Product = require("../db/models/Product");
 module.exports = router;
 
 const requireToken = async (req, res, next) => {
@@ -22,6 +23,52 @@ const requireToken = async (req, res, next) => {
 router.get("/", async (req, res, next) => {
   try {
     const users = await User.findAll();
+    res.json(users);
+  } catch (err) {
+    next(err);
+  }
+});
+
+
+// CUSTOMERS GET /api/users/customers
+// includes all Cart/Order/Product information
+router.get("/customers", async (req, res, next) => {
+  try {
+    const users = await User.findAll({
+      where: {
+        role: "CUST"
+      },
+      include: [{
+        model: Cart,
+        as: "userCart",
+        include: {
+          model: Product,
+          as: "cartProducts"
+        }
+      },
+      {
+        model: Order,
+        as: "userOrders",
+        include: {
+          model: Product,
+          as: "orderProducts"
+        }
+      }]
+    });
+    res.json(users);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// ADMIN GET /api/users/admin
+router.get("/admin", async (req, res, next) => {
+  try {
+    const users = await User.findAll({
+      where: {
+        role: "ADMIN"
+      }
+    });
     res.json(users);
   } catch (err) {
     next(err);
