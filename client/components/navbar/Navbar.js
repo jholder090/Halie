056694223dacf8e1a1364fecb0e1f5538be64454
Joe Holder <jholder090@yahoo.com>
2023-03-31 11:07:41 -0600

    updated navbar cart display slightly

--- client/components/navbar/Navbar.js
+++ client/components/navbar/Navbar.js
@@ -39,7 +39,11 @@ const Navbar = () => {
     }>
       <img src='https://i.postimg.cc/9Mw08wks/lotion.png' alt='Halie Logo' className='h-12' />
       <nav>
-      <small>({getVisitorCartSize(visitorCart)})</small>
+        {visitorCart.length ?
+        <small>({getVisitorCartSize(visitorCart)})</small>
+        :
+        null
+         }
         <button className='md:hidden'>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
             <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
