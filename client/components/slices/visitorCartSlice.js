8ceb51ec9e92c2dea1f49718fed11280653c5296
Joe Holder <jholder090@yahoo.com>
2023-03-31 10:45:15 -0600

    visitor addToCart fully working

--- client/components/slices/visitorCartSlice.js
+++ client/components/slices/visitorCartSlice.js
@@ -8,33 +8,33 @@ const visitorCartSlice = createSlice({
   reducers: {
     addToCart(state, action) {
       console.log("ADD CART PAYLOAD", action.payload)
-      // const itemInCart = state.find((item) => item.id === action.payload.id);
-      // if (itemInCart) {
-      //   itemInCart.quantity++;
-      // } else {
-      //   state.push({ ...action.payload, quantity: 1 });
-      // }
-      // localStorage.setItem("local", JSON.stringify(state))
+      const itemInCart = state.find((item) => item.id === action.payload.id);
+      if (itemInCart) {
+        itemInCart.quantity++;
+      } else {
+        state.push({ ...action.payload, quantity: 1 });
+      }
+      localStorage.setItem("local", JSON.stringify(state))
     },
-    // increase(state, action) {
-    //   const item = state.find((item) => item.id === action.payload);
-    //   item.quantity++;
-    //   localStorage.setItem("local", JSON.stringify(state))
-    // },
-    // decrease(state, action) {
-    //   const item = state.find((item) => item.id === action.payload);
-    //   if (item.quantity === 1) {
-    //     item.quantity = 1;
-    //   } else {
-    //     item.quantity--;
-    //   }
-    //   localStorage.setItem("local", JSON.stringify(state))
-    // },
-    // remove: (state, action) => {
-    //   const filteredItems = state.filter((item) => item.id !== action.payload);
-    //   localStorage.setItem("local", JSON.stringify(filteredItems))
-    //   return filteredItems;
-    // },
+    increase(state, action) {
+      const item = state.find((item) => item.id === action.payload);
+      item.quantity++;
+      localStorage.setItem("local", JSON.stringify(state))
+    },
+    decrease(state, action) {
+      const item = state.find((item) => item.id === action.payload);
+      if (item.quantity === 1) {
+        item.quantity = 1;
+      } else {
+        item.quantity--;
+      }
+      localStorage.setItem("local", JSON.stringify(state))
+    },
+    remove: (state, action) => {
+      const filteredItems = state.filter((item) => item.id !== action.payload);
+      localStorage.setItem("local", JSON.stringify(filteredItems))
+      return filteredItems;
+    },
   },
 });
 export default visitorCartSlice.reducer;
