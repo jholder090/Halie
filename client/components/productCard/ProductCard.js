c63af93e4947da644b0b0d3082f41bd05f6485d7
Joe Holder <jholder090@yahoo.com>
2023-04-22 13:44:16 -0600

    singleProduct basic layout and userButton functionality laid out

--- client/components/productCard/ProductCard.js
+++ client/components/productCard/ProductCard.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { addToUserCartAsync, adjustQtyAsync } from "../slices/userCartSlice";
+import { fetchUserCartAsync, selectUserCart } from "../slices/userCartSlice";
 import { addToVisitorCart } from "../slices/visitorCartSlice";
 import { ShoppingCartSimple } from "phosphor-react";
 import "./productCard.css"
@@ -9,6 +11,7 @@ const ProductCard = ({ user, product, userCart, setAdded }) => {
   const [isHover, setHover] = useState(false);
   const [qty, setQty] = useState(1)
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const isLoggedIn = useSelector((state) => !!state.auth.me.id);
 
 
@@ -43,24 +46,20 @@ const ProductCard = ({ user, product, userCart, setAdded }) => {
 
   return (
     <div onMouseEnter={handleHover} onMouseLeave={handleHover} className={`allProducts-productContainer min-h-fit max-w-sm  mx-4 my-6 flex-col items-center flex-25 border-gray-200 rounded-sm shadow  dark:border-gray-700`} key={product.id}>
-      <div className='allProducts-productImageUrl flex flex-col justify-center'>
-        <a href="#" className='w-full flex justify-center max-h-72'>
-          <img className={isHover ? "rounded-t-lg transition-all duration-700 scale-100" : "rounded-t-lg transition-all duration-700 scale-75"} src={product.imageUrl} alt="Blah" />
-        </a>
+      <div onClick={() => navigate(`/products/${product.id}`)} className='allProducts-productImageUrl flex flex-col justify-center hover:cursor-pointer'>
+        <img className={isHover ? "rounded-t-lg transition-all duration-700 scale-100" : "rounded-t-lg transition-all duration-700 scale-75"} src={product.imageUrl} alt="Blah" />
         <div className='allProducts-productName'>
-          <a href="#">
-            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{product.name}</h5>
-          </a>
+          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{product.name}</h5>
         </div>
       </div>
       {isLoggedIn
         ?
         <div id={product.id}
-          href="#" className="allProducts-buyButton overflow-hidden  h-12 z-50 text-sm font-medium text-center text-white bg-halie-light focus:ring-4 focus:outline-none flex justify-between" >
+          className="allProducts-buyButton overflow-hidden  h-12 z-50 text-sm font-medium text-center text-white bg-halie-light focus:ring-4 focus:outline-none flex justify-between" >
           <div className="div1 m-1 w-4/12 flex items-center justify-around rounded-full bg-halie-dark hover:bg-halie-hover hover:cursor-pointer" onClick={() => addToUserCart(product)}>
-            <ShoppingCartSimple size={28}/>
+            <ShoppingCartSimple size={28} />
             <div>${product.price * qty}</div>
-            </div>
+          </div>
           {/* <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" ><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg> */}
           <div className={isHover ? 'w-36 flex justify-around items-center transition-all duration-700'
             :
@@ -75,7 +74,7 @@ const ProductCard = ({ user, product, userCart, setAdded }) => {
         :
         <div id={product.id} onMouseEnter={handleHover} onMouseLeave={handleHover}
           onClick={() => dispatch(addToVisitorCart(product))}
-          href="#" className="allProducts-buyButton overflow-hidden w-85 px-3 py-2 z-50 text-sm font-medium text-center text-white bg-blue-700  hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          className="allProducts-buyButton overflow-hidden w-85 px-3 py-2 z-50 text-sm font-medium text-center text-white bg-blue-700  hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
           Buy now
           {/* <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" ><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg> */}
           <button className={isHover ? `h-full w-full transition-all duration-700` : `h-full w-full transition-all duration-700 translate-x-full`}>HELLO!</button>
--- client/components/singleProduct/SingleProduct.js
+++ client/components/singleProduct/SingleProduct.js
@@ -2,23 +2,83 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { fetchSingleProductAsync, selectSingleProduct } from "../slices/singleProductSlice";
+import { addToUserCartAsync, adjustQtyAsync } from "../slices/userCartSlice";
 import { fetchUserCartAsync, selectUserCart } from "../slices/userCartSlice";
+import { ShoppingCartSimple } from "phosphor-react";
 
 
 const SingleProduct = () => {
+  const [isHover, setHover] = useState(false);
+  const [qty, setQty] = useState(1)
   const dispatch = useDispatch();
   const { productId } = useParams();
   const product = useSelector(selectSingleProduct);
+  const user = useSelector(state => state.auth.me);
+  let userCart = useSelector(selectUserCart)
 
 
   useEffect(() => {
     dispatch(fetchSingleProductAsync(productId))
   }, [])
 
+  useEffect(() => {
+    dispatch(fetchUserCartAsync(user.id))
+  }, [user])
+
+
+  const handleHover = () => {
+    setHover(!isHover);
+  }
+
+  const adjustQty = (e) => {
+    if (e.target.id == 'decrease' && qty >= 2) {
+      setQty(qty - 1)
+    } else if (e.target.id == 'increase') {
+      setQty(qty + 1)
+    }
+  }
+
+  const addToUserCart = (product) => {
+    for (let i = 0; i < userCart.length; i++) {
+      let item = { ...userCart[i] }
+      if (item.productId === product.id) {
+        item.quantity += qty;
+        item.price = item.product.price * item.quantity
+        return dispatch(adjustQtyAsync(item))
+      }
+    }
+    let cartId = user.id;
+    let quantity = qty;
+    let productId = product.id;
+    let price = product.price * quantity;
+    dispatch(addToUserCartAsync({ cartId, productId, quantity, price }))
+  }
+
   return (
-
-      <div>{product.name}</div>
-
+    <>
+      <div className="singleProduct-top flex border-solid border-2 border-black">
+        <div className="singleProduct-productImages w-1/2 border-solid border-2 border-cyan-300">
+          <img src={product.imageUrl} alt="Blah" />
+          </div>
+        <div className="singleProduct-addToCart w-1/2 border-solid border-2 border-emerald-800">
+          <div>${product.price * qty}</div>
+          <div className={isHover ? 'w-36 flex justify-around items-center transition-all duration-700'
+            :
+            'w-36 flex justify-around items-center transition-all duration-700 translate-x-full'}>
+            <button id='decrease' className='bg-halie-dark hover:bg-halie-hover text-white font-bold py-2 px-5 rounded-full'
+              onClick={(e) => adjustQty(e)}>-</button>
+            <div>{qty}</div>
+            <button id='increase' className='bg-halie-dark hover:bg-halie-hover text-white font-bold py-2 px-5 rounded-full'
+              onClick={(e) => adjustQty(e)}>+</button>
+          </div>
+          <div className="div1 m-1 w-4/12 flex items-center justify-around rounded-full bg-halie-dark hover:bg-halie-hover hover:cursor-pointer" onClick={() => addToUserCart(product)}>
+            <ShoppingCartSimple size={28} />
+            <div>${product.price * qty}</div>
+          </div>
+        </div>
+      </div>
+      <div className="singleProduct-description border-solid border-2 border-red-500">Description</div >
+    </>
 
   );
 };
