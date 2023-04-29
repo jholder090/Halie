ca5e0aba1e1e100e653c58fb100e43f8569a5312
Joe Holder <jholder090@yahoo.com>
2023-04-28 22:27:31 -0600

    successfully created stripe promise, payment intent, and client secret

--- client/App.js
+++ client/App.js
@@ -2,34 +2,36 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import AppRoutes from './AppRoutes';
 import { Navbar, Footer } from "./components/index";
+// Elements here allows us to use elements, components, and gives access to ResolveStripe object within nested components below
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 
 
 
 const App = () => {
+  // Create Stripe object
   const [stripePromise, setStripePromise] = useState(null);
-
+  // Initialize elements
   const [clientSecret, setClientSecret] = useState('');
 
   useEffect(() => {
-
-    
+    // Promise that resolves to a Stripe object
+    // Uses publishable key as an argument
     setStripePromise(loadStripe(`${process.env.STRIPE_PUBLISHABLE_KEY}`));
   }, []);
 
+  // Create payment intent using client secret from server
   useEffect(() => {
     async function fetchData() {
       const { data } = await axios.post('/stripe/client-secret', {
-
       })
-      setClientSecret(data);
-
+      const { clientSecret } = data;
+      setClientSecret(clientSecret);
     }
     fetchData();
   }, [])
 
-
+  // options={{ clientSecret }}
 
   return (
     <>
--- server/stripe.js
+++ server/stripe.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const router = require("express").Router();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_API_KEY)
 module.exports = router;
