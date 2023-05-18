febdd73177474fdecd77c6d7e59ec75548788784
Joe Holder <jholder090@yahoo.com>
2023-05-18 10:58:33 -0600

    working on visitor cart; need to update Redux state appropriately

--- client/components/cart/Cart.js
+++ client/components/cart/Cart.js
@@ -3,6 +3,9 @@ import { useParams, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchUserCartAsync, removeCartItemAsync, selectUserCart } from "../slices/userCartSlice";
 import CartItem from "./CartItem";
+import {
+  remove,
+} from '../slices/visitorCartSlice'
 import './cart.css'
 
 const Cart = () => {
@@ -10,8 +13,11 @@ const Cart = () => {
   const navigate = useNavigate();
   const { cartId } = useParams();
   const user = useSelector(state => state.auth.me);
-  let userCart = useSelector(selectUserCart)
-  // console.log("User Cart: ", userCart)
+  let userCart = useSelector(selectUserCart);
+  console.log("USER CART: ", userCart)
+  const isLoggedIn = useSelector((state) => !!state.auth.me.id);
+  const visitorCart = useSelector(state => state.visitorCart);
+  console.log("Visitor cart cartItem: ", visitorCart)
 
 
 
@@ -43,27 +49,75 @@ const Cart = () => {
     }
   }
 
+  const getVisitorCartSize = (cart) => {
+    let size = 0;
+    for (let product of cart) {
+      size += product.quantity;
+    }
+    return size;
+  }
+
+  const getVisitorCartTotal = (visitorCart) => {
+    // for (let item of visitorCart) {
+    //   console.log("visitor item price: ", item.price)
+    // }
+    let total = 0;
+    for (let item of visitorCart) {
+      total += (item.price * item.quantity);
+    }
+    return (Math.round((total) * 100) / 100).toFixed(2);
+  }
+
+  const clearVisitorCart = () => {
+    for (let item of visitorCart) {
+      dispatch(remove(item.id));
+    }
+  }
+
   return (
     <>
       <div className='cart w-full flex px-12 py-12'>
-        <div className='cart__cartList flex flex-col w-8/12 px-4 justify-between  '>
-          {userCart.map((item, idx) => {
-            return (
-              <CartItem key={idx} item={item} />
-            )
-          })}
-          <button className='cart__clearCart underline' onClick={() => clearCartAsync()}>Clear Cart</button>
-        </div>
-
-
-        <div className='cart__checkout flex flex-col items-center w-4/12'>
-          <div className='cart__cartTotal w-full flex justify-between border-b border-solid border-border-gray'>
-            <div className='cart__itemCount'>{`${getUserCartSize(userCart)} Items`}</div>
-            <div className='cart__totalPrice'>{userCart && userCart.length ? getCartTotal(userCart) : ''}</div>
+        {isLoggedIn ?
+          <div className='cart__cartList flex flex-col w-8/12 px-4 justify-between  '>
+            {userCart.map((item, idx) => {
+              return (
+                <CartItem key={idx} item={item} />
+              )
+            })}
+            <button className='cart__clearCart underline' onClick={() => clearCartAsync()}>Clear Cart</button>
           </div>
-          <div className='cart__checkoutButton flex justify-center items-center h-10 w-1/2 my-6 rounded-full border border-solid bg-halie-light hover:cursor-pointer' onClick={() => navigate(`/checkout`)}>CHECKOUT</div>
-          <button className='cart__continueShopping underline' onClick={() => navigate('/products')}>Continue Shopping</button>
-        </div>
+          :
+          <div className='cart__cartList flex flex-col w-8/12 px-4 justify-between  '>
+            {visitorCart && visitorCart.map((item, idx) => {
+              return (
+                <CartItem key={idx} item={item} />
+              )
+            })}
+            <button className='cart__clearCart underline' onClick={() => clearVisitorCart()}>Clear Cart</button>
+          </div>
+        }
+
+        {isLoggedIn ?
+          <div className='cart__checkout flex flex-col items-center w-4/12'>
+            <div className='cart__cartTotal w-full flex justify-between border-b border-solid border-border-gray'>
+              <div className='cart__itemCount'>{`${getUserCartSize(userCart)} Items`}</div>
+              <div className='cart__totalPrice'>{userCart && userCart.length ? getCartTotal(userCart) : ''}</div>
+            </div>
+            <div className='cart__checkoutButton flex justify-center items-center h-10 w-1/2 my-6 rounded-full border border-solid bg-halie-light hover:cursor-pointer' onClick={() => navigate(`/checkout`)}>CHECKOUT</div>
+            <button className='cart__continueShopping underline' onClick={() => navigate('/products')}>Continue Shopping</button>
+          </div>
+          :
+          <div className='cart__checkout flex flex-col items-center w-4/12'>
+            <div className='cart__cartTotal w-full flex justify-between border-b border-solid border-border-gray'>
+              <div className='cart__itemCount'>{`${getVisitorCartSize(visitorCart)} Items`}</div>
+              <div className='cart__totalPrice'>{visitorCart && visitorCart.length ? getVisitorCartTotal(visitorCart) : ''}</div>
+            </div>
+            <div className='cart__checkoutButton flex justify-center items-center h-10 w-1/2 my-6 rounded-full border border-solid bg-halie-light hover:cursor-pointer' onClick={() => navigate(`/checkout`)}>CHECKOUT</div>
+            <button className='cart__continueShopping underline' onClick={() => navigate('/products')}>Continue Shopping</button>
+          </div>
+        }
+
+
       </div>
 
 
--- client/components/cart/CartItem.js
+++ client/components/cart/CartItem.js
@@ -1,10 +1,17 @@
 import React, { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { adjustQtyAsync, removeCartItemAsync } from "../slices/userCartSlice";
+import {
+  increase,
+  decrease,
+  remove,
+} from '../slices/visitorCartSlice'
 
-
-const CartItem = ({ item,  }) => {
+const CartItem = ({ item, }) => {
+  const isLoggedIn = useSelector((state) => !!state.auth.me.id);
   const dispatch = useDispatch();
+  const visitorCart = useSelector(state => state.visitorCart);
+
 
   const adjustCartQuantityAsync = (e) => {
     let newItem = { ...item };
@@ -26,29 +33,73 @@ const CartItem = ({ item,  }) => {
     dispatch(removeCartItemAsync(item));
   }
 
+  const adjustQty = (e) => {
+    if (e.target.id == "decrease") {
+      if (item.quantity == 1) {
+        dispatch(remove(item.id))
+      } else {
+        dispatch(decrease(item.id))
+      }
+    }
+    if (e.target.id == 'increase') {
+      dispatch(increase(item.id))
+    }
+
+  }
+
   return (
-    <div key={item.id} className='cart_itemContainer flex justify-between h-36 my-2 '>
-      <div className='cart_itemInfo flex justify-center w-2/5'>
-        <div className='cart__itemImage flex w-2/5'>
-          <img src={item.product.imageUrl} className='' />
+    <>
+      {isLoggedIn ?
+        <div className='cart_itemContainer flex justify-between h-36 my-2 '>
+          <div className='cart_itemInfo flex justify-center w-2/5'>
+            <div className='cart__itemImage flex w-2/5'>
+              <img src={item.product.imageUrl} className='' />
+            </div>
+            <div className='cart__itemText flex flex-col justify-center w-2/5'>
+              <div className='cart__itemName my-2'>{item.product.name}</div>
+              <div className='cart__itemPrice my-2'>{item.product.price}</div>
+            </div>
+          </div>
+          <div className='cart_itemQty flex justify-around items-center w-2/5 mr-6'>
+            <div className="cart__adjustQty flex justify-between items-center h-10 w-1/2 my-6 rounded-full border border-solid border-black">
+              <button id='decrease' className=' text-black font-bold py-2 px-5 rounded-full' onClick={(e) => adjustCartQuantityAsync(e)}
+              >-</button>
+              <div>{item.quantity}</div>
+              <button id='increase' className=' text-black font-bold py-2 px-5 rounded-full' onClick={(e) => adjustCartQuantityAsync(e)}
+              >+</button>
+            </div>
+            <div className='cart__itemTotal'>${(Math.round((item.quantity * item.product.price) * 100) / 100).toFixed(2)}</div>
+            <div className='cart__clearItem cursor-pointer' onClick={() => removeItemAsync(item)}>X</div>
+          </div>
         </div>
-        <div className='cart__itemText flex flex-col justify-center w-2/5'>
-          <div className='cart__itemName my-2'>{item.product.name}</div>
-          <div className='cart__itemPrice my-2'>{item.product.price}</div>
-        </div>
-      </div>
-      <div className='cart_itemQty flex justify-around items-center w-2/5 mr-6'>
-        <div className="cart__adjustQty flex justify-between items-center h-10 w-1/2 my-6 rounded-full border border-solid border-black">
-          <button id='decrease' className=' text-black font-bold py-2 px-5 rounded-full' onClick={(e) => adjustCartQuantityAsync(e)}
-          >-</button>
-          <div>{item.quantity}</div>
-          <button id='increase' className=' text-black font-bold py-2 px-5 rounded-full' onClick={(e) => adjustCartQuantityAsync(e)}
-          >+</button>
-        </div>
-        <div className='cart__itemTotal'>${(Math.round((item.quantity * item.product.price) * 100) / 100).toFixed(2)}</div>
-        <div className='cart__clearItem cursor-pointer' onClick={() => removeItemAsync(item)}>X</div>
-      </div>
-    </div>
+
+        :
+
+        <div key={item.id} className='cart_itemContainer flex justify-between h-36 my-2 '>
+          <div className='cart_itemInfo flex justify-center w-2/5'>
+            <div className='cart__itemImage flex w-2/5'>
+              <img src={item.imageUrl} className='' />
+            </div>
+            <div className='cart__itemText flex flex-col justify-center w-2/5'>
+              <div className='cart__itemName my-2'>{item.name}</div>
+              <div className='cart__itemPrice my-2'>{item.price}</div>
+            </div>
+          </div>
+          <div className='cart_itemQty flex justify-around items-center w-2/5 mr-6'>
+            <div className="cart__adjustQty flex justify-between items-center h-10 w-1/2 my-6 rounded-full border border-solid border-black">
+              <button id='decrease' className=' text-black font-bold py-2 px-5 rounded-full' onClick={(e) => adjustQty(e)}
+              >-</button>
+              <div>{item.quantity}</div>
+              <button id='increase' className=' text-black font-bold py-2 px-5 rounded-full' onClick={() => dispatch(increase(item.id))}
+              >+</button>
+            </div>
+            <div className='cart__itemTotal'>${(Math.round((item.quantity * item.price) * 100) / 100).toFixed(2)}</div>
+            <div className='cart__clearItem cursor-pointer' onClick={() => dispatch(remove(item.id))}>X</div>
+          </div>
+        </div>}
+    </>
+
+
   )
 }
 
